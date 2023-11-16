a1ad4af0c4a54664460b806f28a4f6e4341ed891
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-11-16 14:36:58 +0100

    Fix : rewrite route in the web file

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +14,6 @@ use App\Http\Controllers\ArticleController;
 |
 */
 
-route::resource(['articles' => ArticleController::class]);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
